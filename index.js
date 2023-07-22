0eac587ee07389d0a982bfb283ccfdb460afd06d
“ByronO” <byronortizrojas0998@gmail.com>
2023-07-22 00:53:40 -0600

    Configure database connection

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 require('dotenv').config();
+const { dbConnection } = require('./database/config');
 
-
+//express server
 const app = express();
+
+//database
+dbConnection()
 
 //Public
 app.use(express.static('public'));
@@ -12,8 +16,6 @@ app.use(express.json());
 
 //Routes
 app.use('/api/auth', require('./routes/auth'));
-
-
 
 
 app.listen(process.env.PORT, () => {
