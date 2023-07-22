76ee66ee5e242cf0d155f6e919f7f9ca33a4d965
“ByronO” <byronortizrojas0998@gmail.com>
2023-07-22 00:57:06 -0600

    Create User schema and basic registration functionality with encrypted password and field validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,32 +1,55 @@
-const { response } = require('express');
+const { response } = require("express");
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
 
-const createUser = (req, res = response) => {
-
+const createUser = async (req, res = response) => {
     const { name, email, password } = req.body;
 
-    res.send({ 
-        ok: true, 
-        msg: 'register', 
-        name, 
-        email, 
-        password 
-    });
-}
+    try {
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).send({
+                ok: false,
+                msg: "Email already exists",
+            });
+        }
+
+        user = new User(req.body);
+
+        //Encrypt password
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(password, salt);
+
+        await user.save();
+
+        res.status(201).send({
+            ok: true,
+            uid: user.id,
+            name: user.name,
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({
+            ok: false,
+            msg: "Email already exists",
+        });
+    }
+};
 
 const login = (req, res = response) => {
-
     const { email, password } = req.body;
 
-    res.send({ 
-        ok: true, 
-        msg: 'login', 
-        email, 
-        password 
+    res.send({
+        ok: true,
+        msg: "login",
+        email,
+        password,
     });
-}
+};
 
 const renewToken = (req, res = response) => {
-    res.send({ ok: true, msg: 'renew' });
-}
+    res.send({ ok: true, msg: "renew" });
+};
 
-module.exports = { createUser, login, renewToken }
+module.exports = { createUser, login, renewToken };
